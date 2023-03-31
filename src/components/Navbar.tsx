6e10a59909c5591c6abf48f5ad92c8f1ded488cd
Matthew <matthewjcrosby1@gmail.com>
2023-03-31 16:59:47 +0700

    Cleanup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,26 +6,46 @@ import {
   Container,
   Circle,
   Image,
+  useMediaQuery,
+  useDisclosure,
 } from '@chakra-ui/react';
 import React from 'react';
 import shopcart from '../assets/shopping-cart.svg';
 
 function Navbar() {
+  const [isSmallerThan1200] = useMediaQuery('(max-width: 1200px)');
+  const [isSmallerThan700] = useMediaQuery('(max-width: 700px)');
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <HStack
       as="nav"
       background="blackAlpha.900"
       width="100%"
       minH="20%"
-      px="15rem"
+      px={{ md: '2rem', lg: '2rem', xl: '10rem', '2xl': '15rem' }}
+      py={isSmallerThan1200 ? '1rem' : '0rem'}
       color="cyan.100"
       align="center"
+      flexDir={isSmallerThan1200 ? 'column' : 'row'}
     >
-      <Heading fontSize="4rem" color="cyan.100" mr="auto" fontWeight="bold">
+      <Heading
+        fontSize={isSmallerThan700 ? '2.7rem' : '4rem'}
+        color="cyan.100"
+        mr="auto"
+        fontWeight="bold"
+        margin={isSmallerThan1200 ? 'auto' : ''}
+      >
         UnrealStore
       </Heading>
-      <HStack>
-        <HStack as={List} fontSize="2rem" spacing="2rem" mr="5rem">
+      <HStack flexDir={isSmallerThan700 ? 'column' : 'row'}>
+        <HStack
+          as={List}
+          color="white"
+          fontSize={isSmallerThan700 ? '1.5rem' : '2rem'}
+          spacing="2rem"
+          mr={isSmallerThan700 ? '1.5rem' : '5rem'}
+          mb={isSmallerThan700 ? '1.2rem' : '0.5rem'}
+        >
           <ListItem cursor="pointer">Home</ListItem>
           <ListItem cursor="pointer">Products</ListItem>
           <ListItem cursor="pointer">Contact</ListItem>
@@ -33,10 +53,11 @@ function Navbar() {
         <Image
           src={shopcart}
           borderRadius="full"
-          boxSize="70px"
+          boxSize={isSmallerThan700 ? '55px' : '70px'}
           background="white"
           padding="0.8rem"
           cursor="pointer"
+          onClick={() => isOpen}
         />
       </HStack>
     </HStack>
---
+++ src/components/ShoppingCart.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  DrawerFooter,
+  Button,
+} from '@chakra-ui/react';
+
+function ShoppingCart({
+  isOpen,
+  onClose,
+  btnRef,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+  btnRef: React.RefObject<HTMLButtonElement>;
+}) {
+  return (
+    <Drawer
+      isOpen={isOpen}
+      placement="right"
+      onClose={onClose}
+      finalFocusRef={btnRef}
+    >
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerHeader>Create your account</DrawerHeader>
+
+        <DrawerBody />
+
+        <DrawerFooter>
+          <Button variant="outline" mr={3} onClick={onClose}>
+            Cancel
+          </Button>
+          <Button colorScheme="blue">Save</Button>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+}
+
+export default ShoppingCart;

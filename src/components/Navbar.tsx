778de303891f240bf05df22169ec3a6378e52bf1
Matthew <matthewjcrosby1@gmail.com>
2023-03-31 15:09:56 +0700

    Styling navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,14 @@
-import { Heading, HStack, List, ListItem } from '@chakra-ui/react';
+import {
+  Heading,
+  HStack,
+  List,
+  ListItem,
+  Container,
+  Circle,
+  Image,
+} from '@chakra-ui/react';
 import React from 'react';
+import shopcart from '../assets/shopping-cart.svg';
 
 function Navbar() {
   return (
@@ -15,10 +24,20 @@ function Navbar() {
       <Heading fontSize="4rem" color="cyan.100" mr="auto" fontWeight="bold">
         UnrealStore
       </Heading>
-      <HStack as={List} fontSize="2rem" spacing="2rem">
-        <ListItem cursor="pointer">Home</ListItem>
-        <ListItem cursor="pointer">Products</ListItem>
-        <ListItem cursor="pointer">Contact</ListItem>
+      <HStack>
+        <HStack as={List} fontSize="2rem" spacing="2rem" mr="5rem">
+          <ListItem cursor="pointer">Home</ListItem>
+          <ListItem cursor="pointer">Products</ListItem>
+          <ListItem cursor="pointer">Contact</ListItem>
+        </HStack>
+        <Image
+          src={shopcart}
+          borderRadius="full"
+          boxSize="70px"
+          background="white"
+          padding="0.8rem"
+          cursor="pointer"
+        />
       </HStack>
     </HStack>
   );

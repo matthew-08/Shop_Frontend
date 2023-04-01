30d1e18697cd146d20c1cbf9b2464f19cd383108
Matthew <matthewjcrosby1@gmail.com>
2023-04-01 15:37:49 +0700

    Added in react hook form

--- src/Pages/SignIn/SignIn.tsx
+++ src/Pages/SignIn/SignIn.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import {
   FormControl,
   FormLabel,
@@ -7,17 +8,66 @@ import {
   VStack,
   Text,
   Heading,
+  Link,
+  ButtonGroup,
+  Image,
+  Button,
 } from '@chakra-ui/react';
+import { NavLink } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import wave from './wave.svg';
+
+interface FormState {
+  email: string;
+  password: string;
+}
 
 function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormState>();
+
+  const onSubmit = (data: FormState) => console.log(data);
+
   return (
-    <VStack as="form" maxWidth="300px" m="auto" mt="2rem">
-      <Heading>Sign-up</Heading>
-      <FormControl>
-        <FormLabel>Email</FormLabel>
-        <Input type="email" />
-      </FormControl>
-    </VStack>
+    <>
+      <VStack
+        as="form"
+        maxWidth="400px"
+        m="auto"
+        mt="7rem"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Heading>Sign-In</Heading>
+        <FormControl>
+          <FormLabel>Email</FormLabel>
+          <Input type="email" {...register('email')} />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Password</FormLabel>
+          <Input type="text" {...register('password')} />
+        </FormControl>
+        <Text>
+          Don't have an account?{' '}
+          <Text to="/" color="blue.400" as={NavLink}>
+            Sign up here.
+          </Text>
+        </Text>
+        <ButtonGroup>
+          <Button
+            backgroundColor="cyan.100"
+            size="lg"
+            fontSize="1.4rem"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </ButtonGroup>
+      </VStack>
+      <Image />
+    </>
   );
 }
 

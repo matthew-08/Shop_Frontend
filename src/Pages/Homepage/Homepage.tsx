66b6853f5d866899519d5de386fc7d81202a98aa
Matthew <matthewjcrosby1@gmail.com>
2023-04-05 14:14:25 +0700

    Added wave svg to homepage

--- src/Pages/Homepage/Homepage.tsx
+++ src/Pages/Homepage/Homepage.tsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { v4 as uuid } from 'uuid';
 import { Navigation } from 'swiper';
 import CARO_IMAGES from '../../utils/carouselImages';
+import wave from './wave.svg';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -11,46 +12,70 @@ import 'swiper/css/navigation';
 function Homepage() {
   const carouselImages = Object.values(CARO_IMAGES);
   return (
-    <Flex justify="center" mt="4rem">
-      <Flex mr="2rem" textAlign="left" flexDir="column">
-        <Text fontSize="1.9rem" color="gray.800" mb="2rem">
-          An unbelievable online store.
-        </Text>
-        <Heading fontSize="6rem" fontFamily="Poppins">
-          This is
-          <br />
-          not a real
-          <br />
-          store
-        </Heading>
-        <Button>Shop now!</Button>
+    <Flex flexDir="column" overflow="hidden" position="relative">
+      <Flex justify="center" mt="4rem">
+        <Flex mr="5rem" textAlign="left" flexDir="column">
+          <Text fontSize="1.9rem" color="gray.800" mb="2rem">
+            An unbelievable online store.
+          </Text>
+          <Heading fontSize="6rem" fontFamily="Poppins">
+            This is
+            <br />
+            not a real
+            <br />
+            store
+          </Heading>
+          <Button
+            padding="3rem"
+            mt="auto"
+            mb="1.2rem"
+            fontFamily="Poppins"
+            background="blackAlpha.900"
+            color="white"
+            _hover={{
+              backgroundColor: 'blackAlpha.800',
+            }}
+            fontSize="2.5rem"
+            borderRadius="40px"
+          >
+            Shop now!
+          </Button>
+        </Flex>
+        <Flex maxW="40%">
+          <Swiper navigation modules={[Navigation]}>
+            {carouselImages.map((imgSrc) => {
+              return (
+                <SwiperSlide
+                  key={uuid()}
+                  style={{
+                    borderRadius: '10px',
+                  }}
+                >
+                  <Flex width="100%" height="100%">
+                    <Image
+                      src={imgSrc}
+                      borderRadius="30px"
+                      objectFit="cover"
+                      maxH="540px"
+                      width="100%"
+                      height="100%"
+                    />
+                  </Flex>
+                  ...
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </Flex>
       </Flex>
-      <Flex maxW="40%">
-        <Swiper navigation modules={[Navigation]}>
-          {carouselImages.map((imgSrc) => {
-            return (
-              <SwiperSlide
-                key={uuid()}
-                style={{
-                  borderRadius: '10px',
-                }}
-              >
-                <Flex width="100%" height="100%">
-                  <Image
-                    src={imgSrc}
-                    borderRadius="30px"
-                    objectFit="cover"
-                    maxH="540px"
-                    width="100%"
-                    height="100%"
-                  />
-                </Flex>
-                ...
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-      </Flex>
+      <Image
+        src={wave}
+        position="fixed"
+        bgPosition="absolute"
+        zIndex="-1"
+        transform="scaleX(-1)"
+        bottom={-40}
+      />
     </Flex>
   );
 }

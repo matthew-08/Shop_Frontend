3659f9ec5d799a197572a7bbba0ef7d179a1e585
Matthew <matthewjcrosby1@gmail.com>
2023-04-05 13:02:59 +0700

    Add in carousel images utils

--- src/Pages/Homepage/Homepage.tsx
+++ src/Pages/Homepage/Homepage.tsx
@@ -1,7 +1,29 @@
+import { Flex } from '@chakra-ui/react';
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <Flex width="40%" m="auto">
+      <Swiper
+        pagination
+        spaceBetween={50}
+        modules={[Pagination]}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        ...
+      </Swiper>
+    </Flex>
+  );
 }
 
 export default Homepage;

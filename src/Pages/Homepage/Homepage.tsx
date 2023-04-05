108a5964bcbd59696788f8924011eb250b181f5e
Matthew <matthewjcrosby1@gmail.com>
2023-04-05 13:30:18 +0700

    Styling homepage

--- src/Pages/Homepage/Homepage.tsx
+++ src/Pages/Homepage/Homepage.tsx
@@ -1,27 +1,56 @@
-import { Flex } from '@chakra-ui/react';
+import { Button, Flex, Heading, Image, Text, VStack } from '@chakra-ui/react';
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper';
+import { v4 as uuid } from 'uuid';
+import { Navigation } from 'swiper';
+import CARO_IMAGES from '../../utils/carouselImages';
 
 import 'swiper/css';
-import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 function Homepage() {
+  const carouselImages = Object.values(CARO_IMAGES);
   return (
-    <Flex width="40%" m="auto">
-      <Swiper
-        pagination
-        spaceBetween={50}
-        modules={[Pagination]}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        ...
-      </Swiper>
+    <Flex justify="center" mt="4rem">
+      <Flex mr="2rem" textAlign="left" flexDir="column">
+        <Text fontSize="1.9rem" color="gray.800" mb="2rem">
+          An unbelievable online store.
+        </Text>
+        <Heading fontSize="6rem" fontFamily="Poppins">
+          This is
+          <br />
+          not a real
+          <br />
+          store
+        </Heading>
+        <Button>Shop now!</Button>
+      </Flex>
+      <Flex maxW="40%">
+        <Swiper navigation modules={[Navigation]}>
+          {carouselImages.map((imgSrc) => {
+            return (
+              <SwiperSlide
+                key={uuid()}
+                style={{
+                  borderRadius: '10px',
+                }}
+              >
+                <Flex width="100%" height="100%">
+                  <Image
+                    src={imgSrc}
+                    borderRadius="30px"
+                    objectFit="cover"
+                    maxH="540px"
+                    width="100%"
+                    height="100%"
+                  />
+                </Flex>
+                ...
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </Flex>
     </Flex>
   );
 }
